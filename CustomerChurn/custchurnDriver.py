7a28b785d21e624b89e5184a1e2c93f9a4b11e25
Thomas Joseph <joevad@gmail.com>
2020-01-13 11:05:36 +0530

    Committing the driver program

---
+++ CustomerChurn/custchurnDriver.py
@@ -0,0 +1,19 @@
+'''
+JMJPFU
+13-Jan-2020
+This is the library files for the customer churn use case.
+Lord bless this attempt of yours
+'''
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+import argparse
+import sys
+
+print('Lord and my God bless this attempt of yours')
+
+# Getting the details of the arguments
+print(sys.argv)
